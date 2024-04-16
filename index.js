7312e0c33a22c6c5813a7a9f3452473a85edcc9c
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-04-16 11:35:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+var returnResponse = "";
+
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+	if (distanceInFeet > 2500) {
+		returnResponse = 'No can do.';
+	}
+	else if (distanceInFeet > 2000) {
+		returnResponse = 'I will gladly take your thirty bucks.';
+	}
+	else if (distanceInFeet <= 400) {
+		returnResponse = 'This one is on me!';
+	}
+  else if(distanceInFeet > 400 && distanceInFeet < 2000){
+    returnResponse = 'That will be twenty bucks.'
+  }
+	return returnResponse;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(cityName){
   // Write your code here!
+	cityName === 'NYC' ? returnResponse = 'Ok, sounds good.' : returnResponse = 'No go.';
+	return returnResponse;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+	switch (tipAmount) {
+		case 'generous' : returnResponse = 'Thank you so much.'; break;
+		case 'not as generous': returnResponse = 'Thank you.'; break;
+		default : returnResponse = 'Bye.';
+	}
+	return returnResponse;
 }
